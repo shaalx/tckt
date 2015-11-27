8e6bf49bfa67f2bb33d813dea113fee6ab8d3b8c
shaalx <shaalx@yahoo.com.sg>
2015-11-27 21:23:36 +0800

    init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shaalx/jsnm"
+)
+
+func main() {
+	jm := jsnm.FileNameFmt("city.md")
+	provinces := jm.Get("p").Arr()
+	cities := jm.Get("c").Arr()
+	for i, it := range provinces {
+		fmt.Printf("***** %dth: %v\n", i, it.RawData().String())
+		ccs := cities[i].Arr()
+		for i, it := range ccs {
+			fmt.Printf("%dth: %v\t", i, it.RawData().String())
+		}
+		fmt.Println()
+	}
+
+	code_jm := jsnm.FileNameFmt("code.md")
+	bjc := code_jm.Get("北京").RawData().String()
+	fmt.Printf("Beijing's code:%s\n", bjc)
+}
